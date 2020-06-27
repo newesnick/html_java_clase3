127215a7ada250c43e80e35023f9028c3c99f00c
newesnick <vitto.olave@ug.uchile.cl>
2020-06-27 18:17:50 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const name ="sacm";//string
+let lastname1="asd"; //string
+let lastname2='asd' ;//string
+let lastname3=`asd ${name}`; //string
+let number = 0; //number
+let boolean = true; //boolean
+let array = ["sacm", 0, false];
+let object = {pelo:"bonito", raza:"kiltro",edad:29,};
+console.log(array[2])
+console.log(object.raza)
+console.log(name[2])
+console.log(object.edad)
+let welcome = "javascript"
+
+/* function set_name () {
+    alert('bienvenido a ${welcome}')
+} */
+const set_name = () => {alert(`bienvenido a ${welcome}`)}
